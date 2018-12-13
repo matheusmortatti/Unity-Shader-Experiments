761cb1490947020e5eefdd55d1edbc293c5d0e3b
MatheusMortatti <matheusmortatti@gmail.com>
2018-12-13 13:50:48 -0200

    first commit with a No Mans Sky Scanner style shader

---
+++ Assets/Scripts/ScanEffect.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScanEffect : MonoBehaviour
+{
+    public Transform ScannerOrigin;
+    public float ScanDistance;
+    public float ScanWidth;
+    public float ScanVelocity;
+
+    private Material material;
+    private Camera _camera;
+
+    void Awake()
+    {
+        material = new Material(Shader.Find("Hidden/Scan"));
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            ScanDistance = 0;
+        }
+
+        ScanDistance += Time.deltaTime * ScanVelocity;
+    }
+
+    [ImageEffectOpaque]
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        material.SetVector("_WorldSpaceScannerPos", ScannerOrigin.position);
+        material.SetFloat("_ScanDistance", ScanDistance);
+        material.SetFloat("_ScanWidth", ScanWidth);
+
+        RaycastCornerBlit(source, destination, material);
+    }
+
+    void RaycastCornerBlit(RenderTexture source, RenderTexture dest, Material mat)
+    {
+        // Compute Frustum Corners
+        float camFar = _camera.farClipPlane;
+        float camFov = _camera.fieldOfView;
+        float camAspect = _camera.aspect;
+
+        float fovWHalf = camFov * 0.5f;
+
+        Vector3 toRight = _camera.transform.right * Mathf.Tan(fovWHalf * Mathf.Deg2Rad) * camAspect;
+        Vector3 toTop = _camera.transform.up * Mathf.Tan(fovWHalf * Mathf.Deg2Rad);
+
+        Vector3 topLeft = (_camera.transform.forward - toRight + toTop);
+        float camScale = topLeft.magnitude * camFar;
+
+        topLeft.Normalize();
+        topLeft *= camScale;
+
+        Vector3 topRight = (_camera.transform.forward + toRight + toTop);
+        topRight.Normalize();
+        topRight *= camScale;
+
+        Vector3 bottomRight = (_camera.transform.forward + toRight - toTop);
+        bottomRight.Normalize();
+        bottomRight *= camScale;
+
+        Vector3 bottomLeft = (_camera.transform.forward - toRight - toTop);
+        bottomLeft.Normalize();
+        bottomLeft *= camScale;
+
+        // Custom Blit, encoding Frustum Corners as additional Texture Coordinates
+        RenderTexture.active = dest;
+
+        mat.SetTexture("_MainTex", source);
+
+        GL.PushMatrix();
+        GL.LoadOrtho();
+
+        mat.SetPass(0);
+
+        GL.Begin(GL.QUADS);
+
+        GL.MultiTexCoord2(0, 0.0f, 0.0f);
+        GL.MultiTexCoord(1, bottomLeft);
+        GL.Vertex3(0.0f, 0.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 1.0f, 0.0f);
+        GL.MultiTexCoord(1, bottomRight);
+        GL.Vertex3(1.0f, 0.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 1.0f, 1.0f);
+        GL.MultiTexCoord(1, topRight);
+        GL.Vertex3(1.0f, 1.0f, 0.0f);
+
+        GL.MultiTexCoord2(0, 0.0f, 1.0f);
+        GL.MultiTexCoord(1, topLeft);
+        GL.Vertex3(0.0f, 1.0f, 0.0f);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ Assets/Shaders/Scan.shader
@@ -0,0 +1,82 @@
+﻿Shader "Hidden/Scan"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // No culling or depth
+        Cull Off ZWrite Off ZTest Always
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+				float4 ray : TEXCOORD1;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+				float2 uv : TEXCOORD0;
+				float2 uv_depth : TEXCOORD1;
+				float4 interpolatedRay : TEXCOORD2;
+            };
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+
+				o.uv_depth = v.uv.xy;
+				o.interpolatedRay = v.ray;
+
+                return o;
+            }
+
+            sampler2D _MainTex;
+			sampler2D_float _CameraDepthTexture;
+
+			float4 _WorldSpaceScannerPos;
+			float _ScanDistance;
+			float _ScanWidth;
+
+			float4 horizontalBars(float y)
+			{
+				return 1 - saturate(round(abs(frac(y * 100) * 2)));
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				fixed4 col = tex2D(_MainTex, i.uv);
+
+				float rawDepth = DecodeFloatRG(tex2D(_CameraDepthTexture, i.uv_depth));
+				float linearDepth = Linear01Depth(rawDepth);
+				float4 wsDir = linearDepth * i.interpolatedRay;
+				float3 wsPos = _WorldSpaceCameraPos + wsDir;
+
+				float dist = distance(wsPos, _WorldSpaceScannerPos);
+
+				if (dist < _ScanDistance && dist > _ScanDistance - _ScanWidth && linearDepth < 1)
+				{
+					float scanCol = 1 - (_ScanDistance - dist) / _ScanWidth;
+					col.rgb += scanCol + horizontalBars(i.uv.y);
+				}
+
+                // just invert the colors
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
